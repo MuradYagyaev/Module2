e25624812fc7e5db3e88c56271b84bb6a130457e
MuradYagyaev <165575932+MuradYagyaev@users.noreply.github.com>
2024-05-05 19:04:40 +0300

    Add files via upload

---
+++ module2hard_ver1.py
@@ -0,0 +1,22 @@
+# Дополнительное практическое задание по модулю: "Основные операторы."
+
+# Поиск максимального значения первого числа
+def max_first(n):
+    if n % 2 == 0:
+        return int(n / 2)
+    else:
+        return int(n // 2 + 1)
+
+# Поиск пароля для числа
+def find_password(n):
+    password = ''
+    for i in range(1, max_first(n)):
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
+
+
+# Вывод всех найденных значений
+for i in range(3, 21):
+    print(f'{i} - {find_password(i)}')
